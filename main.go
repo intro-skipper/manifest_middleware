a73a5d90bc5d86e4858cbfe63fbeb0d29ef3f01b
Kilian von Pflugk <github@jumoog.io>
2026-02-21 18:58:08 +0100

    chore: remove placeholder main.go
    The main function was empty and served only as a placeholder. Actual module registration is handled via init() in other files.

--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// This is a placeholder for the build process.
-// The actual module registration happens in manifest_redirect.go and webhook_handler.go via init().
-// To build this module into Caddy, use xcaddy:
-//
-// xcaddy build --with github.com/intro-skipper/manifest_middleware
-func main() {}
